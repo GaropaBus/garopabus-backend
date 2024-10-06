5def45e81b50b991aa97e8ec6f990f32e1063c4e
Natan Konig <93478667+NatanKonig@users.noreply.github.com>
2024-10-06 19:05:54 -0300

    build: fix build

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+// Verificar se está em produção ou desenvolvimento
+const isProduction = process.env.NODE_ENV === "production";
+
+// Definir a porta com base no ambiente
+const PORT = isProduction ? 8022 : (process.env.PORT || 8080);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
